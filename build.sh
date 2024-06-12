f9f9e2ddac1c1bc2bd4a3092bdc04d4b6d6debd7
Diaz1401 <diaznura@outlook.com>
2024-06-12 08:37:12 +0700

    build: Update coding style
    Signed-off-by: Diaz1401 <diaznura@outlook.com>

--- build.sh
+++ build.sh
@@ -14,38 +14,52 @@ OPT_FLAGS="-pipe -O3 -flto=${NPROC} -fipa-pta -fgraphite -fgraphite-identity -fl
 BUILD_DATE="$(cat ${WORK_DIR}/gcc/gcc/DATESTAMP)"
 BUILD_DAY="$(date "+%d %B %Y")"
 BUILD_TAG="$(date +%Y%m%d-%H%M-%Z)"
-# Targets: aarch64-linux-gnu, x86_64-linux-gnu, arm-eabi
+# Targets: aarch64, x86_64, arm-eabi
 TARGETS="aarch64-linux-gnu"
 HEAD_SCRIPT="$(git log -1 --oneline)"
 HEAD_GCC="$(git --git-dir gcc/.git log -1 --oneline)"
 HEAD_BINUTILS="$(git --git-dir binutils/.git log -1 --oneline)"
 PKG_VERSION="CAT"
-IS_MASTER="${1}"
-export PKG_VERSION WORK_DIR NPROC PREFIX OPT_FLAGS BUILD_DATE BUILD_DAY BUILD_TAG TARGETS HEAD_SCRIPT HEAD_GCC HEAD_BINUTILS IS_MASTER
+MASTER=false
+GCC10=false
+for ARGS in $@; do
+    case $ARGS in
+    master)
+        GCC10=false MASTER=true
+        ;;
+    gcc10)
+        GCC10=true MASTER=false
+        ;;
+    *)
+        GCC10=false MASTER=false
+        ;;
+    esac
+done
+export GCC10 PKG_VERSION WORK_DIR NPROC PREFIX OPT_FLAGS BUILD_DATE BUILD_DAY BUILD_TAG TARGETS HEAD_SCRIPT HEAD_GCC HEAD_BINUTILS MASTER
 
-send_info(){
+send_info() {
     curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" \
-    -d chat_id="${CHAT_ID}" \
-    -d "parse_mode=html" \
-    -d text="${1}" > /dev/null 2>&1
+        -d chat_id="${CHAT_ID}" \
+        -d "parse_mode=html" \
+        -d text="${1}" >/dev/null 2>&1
 }
 
-send_file(){
+send_file() {
     curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendDocument" \
-    -F document=@"${1}" \
-    -F chat_id="${CHAT_ID}" \
-    -F "parse_mode=html" \
-    -F caption="${2}" > /dev/null 2>&1
+        -F document=@"${1}" \
+        -F chat_id="${CHAT_ID}" \
+        -F "parse_mode=html" \
+        -F caption="${2}" >/dev/null 2>&1
 }
 
-build_zstd(){
+build_zstd() {
     #  send_info "<b>GitHub Action : </b><pre>Zstd build started . . .</pre>"
     mkdir ${WORK_DIR}/build-zstd
     cd ${WORK_DIR}/build-zstd
-    cmake ${WORK_DIR}/zstd/build/cmake -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" |& tee -a build.log
+    cmake ${WORK_DIR}/zstd/build/cmake -DCMAKE_INSTALL_PREFIX="${PREFIX}" |& tee -a build.log
     make -j${NPROC} |& tee -a build.log
     make install -j${NPROC} |& tee -a build.log
-    
+
     # check Zstd build status
     if [ -f "${PREFIX}/bin/zstd" ]; then
         rm -rf ${WORK_DIR}/build-zstd
@@ -59,40 +73,39 @@ build_zstd(){
     fi
 }
 
-build_binutils(){
+build_binutils() {
     #  send_info "<b>GitHub Action : </b><pre>Binutils build started . . .</pre><b>Target : </b><pre>[${TARGET}]</pre>"
     mkdir ${WORK_DIR}/build-binutils
     cd ${WORK_DIR}/build-binutils
     # Check compiler first
     gcc -v |& tee -a build.log
     ld -v |& tee -a build.log
-    
+
     env CFLAGS="${OPT_FLAGS}" CXXFLAGS="${OPT_FLAGS}" \
-    ../binutils/configure \
-    --disable-compressed-debug-sections \
-    --disable-docs \
-    --disable-gdb \
-    --disable-gold \
-    --disable-gprofng \
-    --disable-multilib \
-    --disable-nls \
-    --disable-shared \
-    --enable-ld=default \
-    --enable-plugins \
-    --enable-threads \
-    --enable-64-bit-bfd \
-    --prefix=${PREFIX} \
-    --program-prefix=${TARGET}- \
-    --target=${TARGET} \
-    --with-pkgversion="${PKG_VERSION} Binutils" \
-    --with-sysroot \
-    --with-system-zlib \
-    --quiet |& tee -a build.log
+        ../binutils/configure \
+        --disable-compressed-debug-sections \
+        --disable-docs \
+        --disable-gdb \
+        --disable-gold \
+        --disable-gprofng \
+        --disable-multilib \
+        --disable-nls \
+        --disable-shared \
+        --enable-ld=default \
+        --enable-plugins \
+        --enable-threads \
+        --enable-64-bit-bfd \
+        --prefix=${PREFIX}/${TARGET} \
+        --target=${TARGET} \
+        --with-pkgversion="${PKG_VERSION} Binutils" \
+        --with-sysroot \
+        --with-system-zlib \
+        --quiet |& tee -a build.log
     make -j${NPROC} |& tee -a build.log
     make install -j${NPROC} |& tee -a build.log
-    
+
     # check Binutils build status
-    if [ -f "${PREFIX}/bin/${TARGET}-ld" ]; then
+    if [ -f "${PREFIX}/${TARGET}/bin/${TARGET}-ld" ]; then
         rm -rf ${WORK_DIR}/build-binutils
         #    send_info "<b>GitHub Action : </b><pre>Binutils build finished ! ! !</pre>"
         cd -
@@ -104,70 +117,70 @@ build_binutils(){
     fi
 }
 
-build_gcc(){
+build_gcc() {
     #  send_info "<b>GitHub Action : </b><pre>GCC build started . . .</pre><b>Target : </b><pre>[${TARGET}]</pre>"
     mkdir ${WORK_DIR}/build-gcc
     cd ${WORK_DIR}/build-gcc
     # Check compiler first
     gcc -v |& tee -a build.log
     ld -v |& tee -a build.log
-    
+
     case $TARGET in
-        x86_64*)
-            EXTRA_CONF="--without-cuda-driver"
+    x86_64*)
+        EXTRA_CONF="--without-cuda-driver"
         ;;
-        aarch64*)
-            EXTRA_CONF="--enable-fix-cortex-a53-835769 \
+    aarch64*)
+        EXTRA_CONF="--enable-fix-cortex-a53-835769 \
             --enable-fix-cortex-a53-843419 \
             --with-headers=/usr/include"
         ;;
-        arm*)
-            EXTRA_CONF="--with-headers=/usr/include"
+    arm*)
+        EXTRA_CONF="--with-headers=/usr/include"
         ;;
     esac
-    
+
     env CFLAGS="${OPT_FLAGS}" CXXFLAGS="${OPT_FLAGS}" \
-    ../gcc/configure \
-    --disable-bootstrap \
-    --disable-checking \
-    --disable-decimal-float \
-    --disable-docs \
-    --disable-gcov \
-    --disable-libcc1 \
-    --disable-libffi \
-    --disable-libgomp \
-    --disable-libquadmath \
-    --disable-libsanitizer \
-    --disable-libssp \
-    --disable-libstdcxx-debug \
-    --disable-libstdcxx-pch \
-    --disable-libvtv \
-    --disable-multilib \
-    --disable-nls \
-    --disable-shared \
-    --enable-default-pie \
-    --enable-default-ssp \
-    --enable-gnu-indirect-function \
-    --enable-languages=c,c++ \
-    --enable-linux-futex \
-    --enable-threads=posix \
-    --prefix=${PREFIX} \
-    --program-prefix=${TARGET}- \
-    --target=${TARGET} \
-    --with-gnu-as \
-    --with-gnu-ld \
-    --with-newlib \
-    --with-pkgversion="${PKG_VERSION} GCC" \
-    --with-sysroot \
-    --with-system-zlib \
-    --quiet ${EXTRA_CONF} |& tee -a build.log
+        ../gcc/configure \
+        --disable-bootstrap \
+        --disable-checking \
+        --disable-decimal-float \
+        --disable-docs \
+        --disable-gcov \
+        --disable-libcc1 \
+        --disable-libffi \
+        --disable-libgomp \
+        --disable-libquadmath \
+        --disable-libsanitizer \
+        --disable-libssp \
+        --disable-libstdcxx-debug \
+        --disable-libstdcxx-pch \
+        --disable-libvtv \
+        --disable-multilib \
+        --disable-nls \
+        --disable-shared \
+        --enable-default-pie \
+        --enable-default-ssp \
+        --enable-gnu-indirect-function \
+        --enable-languages=c,c++ \
+        --enable-linux-futex \
+        --enable-pgo-build \
+        --enable-threads=posix \
+        --prefix=${PREFIX}/${TARGET} \
+        --target=${TARGET} \
+        --with-gnu-as \
+        --with-gnu-ld \
+        --with-newlib \
+        --with-pkgversion="${PKG_VERSION} GCC" \
+        --with-sysroot \
+        --with-system-zlib \
+        --quiet ${EXTRA_CONF} |& tee -a build.log
     make all-gcc -j${NPROC} |& tee -a build.log
     make all-target-libgcc -j${NPROC} |& tee -a build.log
     make install-gcc -j${NPROC} |& tee -a build.log
     make install-target-libgcc -j${NPROC} |& tee -a build.log
-    
+
     # check GCC build status
-    if [ -f "${PREFIX}/bin/${TARGET}-gcc" ]; then
+    if [ -f "${PREFIX}/${TARGET}/bin/${TARGET}gcc" ]; then
         rm -rf ${WORK_DIR}/build-gcc
         #    send_info "<b>GitHub Action : </b><pre>GCC build finished ! ! !</pre>"
         cd -
@@ -179,39 +192,42 @@ build_gcc(){
     fi
 }
 
-strip_binaries(){
+strip_binaries() {
     #  send_info "<b>GitHub Action : </b><pre>Strip binaries . . .</pre>"
-    
-    find install -type f -exec file {} \; > .file-idx
-    
+
+    find install -type f -exec file {} \; >.file-idx
+
     for TARGET in ${TARGETS}; do
         case $TARGET in
-            x86_64*)
-                grep "x86-64" .file-idx |
+        x86_64*)
+            grep "x86-64" .file-idx |
                 grep "not strip" |
                 tr ':' ' ' | awk '{print $1}' |
-                while read -r file
-                do strip -s "$file"
-            done;;
-            aarch64*)
-                cp -rf ${PREFIX}/bin/${TARGET}-strip ./stripp-a64
-                grep "ARM" .file-idx | grep "aarch64" |
+                while read -r file; do
+                    strip -s "$file"
+                done
+            ;;
+        aarch64*)
+            cp -rf ${PREFIX}/${TARGET}/bin/strip ./stripp-a64
+            grep "ARM" .file-idx | grep "aarch64" |
                 grep "not strip" |
                 tr ':' ' ' | awk '{print $1}' |
-                while read -r file
-                do ./stripp-a64 -s "$file"
-            done;;
-            arm*)
-                cp -rf ${PREFIX}/bin/${TARGET}-strip ./stripp-a32
-                grep "ARM" .file-idx | grep "eabi" |
+                while read -r file; do
+                    ./stripp-a64 -s "$file"
+                done
+            ;;
+        arm*)
+            cp -rf ${PREFIX}/${TARGET}/bin/strip ./stripp-a32
+            grep "ARM" .file-idx | grep "eabi" |
                 grep "not strip" |
                 tr ':' ' ' | awk '{print $1}' |
-                while read -r file
-                do ./stripp-a32 -s "$file"
-            done;;
+                while read -r file; do
+                    ./stripp-a32 -s "$file"
+                done
+            ;;
         esac
     done
-    
+
     # clean unused files
     find install -name *.cmake -delete
     find install -name *.la -delete
@@ -219,34 +235,34 @@ strip_binaries(){
     rm -rf stripp-* .file-idx
 }
 
-git_push(){
+git_push() {
     send_info "<b>GitHub Action : </b><pre>Release into GitHub . . .</pre>"
-    GCC_CONFIG="$(${PREFIX}/bin/aarch64-linux-gnu-gcc -v 2>&1)"
-    GCC_VERSION="$(${PREFIX}/bin/aarch64-linux-gnu-gcc --version | head -n1 | cut -d' ' -f4)"
-    BINUTILS_VERSION="$(${PREFIX}/bin/aarch64-linux-gnu-ld --version | head -n1 | cut -d' ' -f5)"
+    GCC_CONFIG="$(${PREFIX}/aarch64-linux-gnu/bin/aarch64-linux-gnu-gcc -v 2>&1)"
+    GCC_VERSION="$(${PREFIX}/aarch64-linux-gnu/bin/aarch64-linux-gnu-gcc --version | head -n1 | cut -d' ' -f4)"
+    BINUTILS_VERSION="$(${PREFIX}/aarch64-linux-gnu/bin/aarch64-linux-gnu-ld --version | head -n1 | cut -d' ' -f5)"
     MESSAGE="GCC: ${GCC_VERSION}-${BUILD_DATE}, Binutils: ${BINUTILS_VERSION}"
-    
+
     # symlink liblto_plugin.so
-    cd ${PREFIX}/lib/bfd-plugins
+    cd ${PREFIX}/aarch64-linux-gnu/lib/bfd-plugins
     ln -sr ../../libexec/gcc/aarch64-linux-gnu/${GCC_VERSION}/liblto_plugin.so .
-    
+
     git config --global user.name github-actions[bot]
     git config --global user.email github-actions[bot]@users.noreply.github.com
-    if [ "${IS_MASTER}" == "master" ]; then
-        git clone https://Diaz1401:"${GITHUB_TOKEN}"@github.com/Mengkernel/gcc ${WORK_DIR}/gcc-repo -b main
+    if ${MASTER}; then
+        git clone https://Diaz1401:${GITHUB_TOKEN}@github.com/Mengkernel/gcc ${WORK_DIR}/gcc-repo -b main
     else
-        git clone https://Diaz1401:"${GITHUB_TOKEN}"@github.com/Diaz1401/gcc-stable ${WORK_DIR}/gcc-repo -b main
+        git clone https://Diaz1401:${GITHUB_TOKEN}@github.com/Diaz1401/gcc-stable ${WORK_DIR}/gcc-repo -b main
     fi
     # Generate archive
     cd ${WORK_DIR}/gcc-repo
     cp -rf ${PREFIX}/* .
-    tar -I"${PREFIX}/bin/zstd --ultra -22 -T0" -cf gcc.tar.zst *
+    tar -I"${PREFIX}/zstd/bin/zstd --ultra -22 -T0" -cf gcc.tar.zst *
     cat README |
-    sed s/GCCVERSION/$(echo ${GCC_VERSION}-${BUILD_DATE})/g |
-    sed s/BINUTILSVERSION/$(echo ${BINUTILS_VERSION})/g > README.md
+        sed s/GCCVERSION/$(echo ${GCC_VERSION}-${BUILD_DATE})/g |
+        sed s/BINUTILSVERSION/$(echo ${BINUTILS_VERSION})/g >README.md
     git commit --allow-empty -as \
-    -m "${MESSAGE}" \
-    -m "${GCC_CONFIG}"
+        -m "${MESSAGE}" \
+        -m "${GCC_CONFIG}"
     git push origin main
     hub release create -a gcc.tar.zst -m "${MESSAGE}" ${BUILD_TAG}
     cd -
--- resources.sh
+++ resources.sh
@@ -4,22 +4,41 @@ echo "*****************************************"
 echo "*        Download GCC & Binutils        *"
 echo "*****************************************"
 
-export IS_MASTER="${1}"
+MASTER=false
+GCC10=false
+for ARGS in $@; do
+    case $ARGS in
+    master)
+        MASTER=true GCC10=false
+        ;;
+    gcc10)
+        GCC10=true MASTER=false
+        ;;
+    *)
+        GCC10=false MASTER=false
+        ;;
+    esac
+done
+export MASTER GCC10
 
 download() {
-    if [ "${IS_MASTER}" == "master" ]; then
-        git clone -b master --depth=1 git://sourceware.org/git/binutils-gdb.git binutils
-        git clone -b master --depth=1 git://gcc.gnu.org/git/gcc.git gcc
-        git clone -b dev https://github.com/facebook/zstd zstd
+    if ${MASTER}; then
+        git clone --depth=1 -b master git://sourceware.org/git/binutils-gdb.git binutils
+        git clone --depth=1 -b master git://gcc.gnu.org/git/gcc.git gcc
+        git clone --depth=1 -b dev https://github.com/facebook/zstd zstd
+    elif ${GCC10}; then
+        git clone --depth=1 -b binutils-2_42-branch https://github.com/Diaz1401/binutils-gdb.git binutils
+        git clone --depth=1 -b releases/gcc-10 https://github.com/Diaz1401/gcc gcc
+        git clone --depth=1 -b v1.5.6 https://github.com/facebook/zstd zstd
     else
-        git clone --depth=1 https://github.com/Diaz1401/binutils-gdb.git binutils
-        git clone --depth=1 https://github.com/Diaz1401/gcc gcc
-        git clone -b v1.5.5 https://github.com/facebook/zstd zstd
+        git clone --depth=1 -b binutils-2_42-branch https://github.com/Diaz1401/binutils-gdb.git binutils
+        git clone --depth=1 -b releases/gcc-13 https://github.com/Diaz1401/gcc gcc
+        git clone --depth=1 -b v1.5.6 https://github.com/facebook/zstd zstd
     fi
     sed -i '/^development=/s/true/false/' binutils/bfd/development.sh
     cd gcc
     ./contrib/download_prerequisites
-    cd ..
+    cd -
 }
 
 download
