587c7a0e4629892eb505a634bd3b15d745f96822
Diaz1401 <diaznura@outlook.com>
2024-06-26 10:37:40 +0700

    resources: Build GCC 11 for release
    Signed-off-by: Diaz1401 <diaznura@outlook.com>

--- resources.sh
+++ resources.sh
@@ -19,20 +19,30 @@ download() {
         git clone --depth=1 -b master git://sourceware.org/git/binutils-gdb.git binutils
         git clone --depth=1 -b master git://gcc.gnu.org/git/gcc.git gcc
         git clone --depth=1 -b dev https://github.com/facebook/zstd zstd
+        cd gcc
+        git apply -3 \
+            ../patches/0001* \
+            ../patches/0002* \
+            ../patches/0003* \
+            || (echo " * Failed to apply patches * " && exit 1)
     else
         git clone --depth=1 -b binutils-2_42-branch git://sourceware.org/git/binutils-gdb.git binutils
-        git clone --depth=1 -b releases/gcc-14 git://gcc.gnu.org/git/gcc.git gcc
+        git clone --depth=1 -b releases/gcc-11 git://gcc.gnu.org/git/gcc.git gcc
         git clone --depth=1 -b v1.5.6 https://github.com/facebook/zstd zstd
+        cd gcc
+        git apply -3 \
+            ../patches/0001* \
+            ../patches/0002* \
+            ../patches/0004* \
+            || (echo " * Failed to apply patches * " && exit 1)
     fi
-    sed -i '/^development=/s/true/false/' binutils/bfd/development.sh
-    cd gcc
-    git apply -3 ../patches/* || (echo " * Failed to apply patches * " && exit 1)
     ./contrib/download_prerequisites
     mkdir -p ../kernel
     cd ../kernel
     git init .
     git remote add origin https://github.com/mengkernel/kernel_xiaomi_sm8250.git
     cd ..
+    sed -i '/^development=/s/true/false/' binutils/bfd/development.sh
 }
 
 download
