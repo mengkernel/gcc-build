8f31f5de4fdc0768ba7645665f147b5b6bf5b84d
Diaz1401 <diaznura@outlook.com>
2024-08-19 13:21:34 +0000

    resources: Release GCC 14 with Binutils v2.43
    Signed-off-by: Diaz1401 <diaznura@outlook.com>

--- resources.sh
+++ resources.sh
@@ -26,14 +26,14 @@ download() {
             ../patches/0003* \
             || (echo " * Failed to apply patches * " && exit 1)
     else
-        git clone --depth=1 -b binutils-2_42-branch git://sourceware.org/git/binutils-gdb.git binutils
-        git clone --depth=1 -b releases/gcc-11 git://gcc.gnu.org/git/gcc.git gcc
+        git clone --depth=1 -b binutils-2_43-branch git://sourceware.org/git/binutils-gdb.git binutils
+        git clone --depth=1 -b releases/gcc-14 git://gcc.gnu.org/git/gcc.git gcc
         git clone --depth=1 -b v1.5.6 https://github.com/facebook/zstd zstd
         cd gcc
         git apply -3 \
-            ../patches/0005* \
+            ../patches/0001* \
             ../patches/0002* \
-            ../patches/0004* \
+            ../patches/0003* \
             || (echo " * Failed to apply patches * " && exit 1)
     fi
     ./contrib/download_prerequisites
